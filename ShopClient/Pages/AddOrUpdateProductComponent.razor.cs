5d11f74c87745d4d9527d5eedbd30687a9baa03e
Kaloyan Kolev <kaloyan.kolev@hotmail.com>
2024-10-29 23:20:44 +0200

    Added a package and some components

---
+++ ShopClient/Pages/AddOrUpdateProductComponent.razor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using PhoneShopSharedLibrary.Models;
+using ShopClient.Pages; // Adjust the namespace as needed
+
+
+namespace ShopClient.Pages
+{
+    public partial class AddOrUpdateProductComponent : ComponentBase
+    {
+
+        Product myProduct = new();
+        string ImageUploadMessage = "";
+        async Task HandleSaveProduct()
+        {
+            ShowSaveButton = false;
+            ShowBusyButton = !ShowSaveButton;
+            if (String.IsNullOrEmpty(myProduct.Base64Img))
+            {
+                messageDialog!.SetDialogValues("warning", "You need to choose image");
+                SetMessageDialog();
+                return;
+            }
+        }
+
+        async Task UploadImage(InputFileChangeEventArgs e)
+        {
+            if (e.File.Name.ToLower().Contains(".png"))
+            {
+                var format = "image/png";
+                var resizeImage = await e.File.RequestImageFileAsync(format, 300, 300);
+                var buffer = new byte[resizeImage.Size];
+                await resizeImage.OpenReadStream().ReadAsync(buffer);
+                var imageData = $"data:{format};base64,{Convert.ToBase64String(buffer)}";
+                ImageUploadMessage = "";
+                myProduct.Base64Img = imageData;
+                imageData = "";
+                return;
+            }
+            ImageUploadMessage = "PNG File needed.";
+            return;
+        }
+        MessageDialog? messageDialog;
+        public bool ShowBusyButton { get; set; }
+        public bool ShowSaveButton { get; set; } = true;
+
+
+        private async void SetMessageDialog()
+        {
+            await messageDialog!.ShowMessage();
+            ShowBusyButton = false;
+            ShowSaveButton = !ShowBusyButton;
+            StateHasChanged();
+        }
+    }
+}
---
+++ ShopClient/Pages/MessageDialog.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace ShopClient.Pages
+{
+    public partial class MessageDialog: ComponentBase
+    {
+        private string AlertType { get; set; } = string.Empty;
+        private string Message { get; set; } = string.Empty ;
+
+        public void SetDialogValues(string alertType, string message)
+        {
+            AlertType = alertType;
+            Message = message;
+            StateHasChanged();
+        }
+        public async Task ShowMessage() => await js.InvokeVoidAsync("ShowDialog");
+    }
+}
